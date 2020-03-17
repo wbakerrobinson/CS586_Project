f018d8facf50eba1f1d17099adc3cc7e93438211
wbakerrobinson <wzb@pdx.edu>
2020-03-16 21:22:28 -0700

    attempting to troubleshoot the markdown

---
+++ libraries_and_functions_shiny.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(shinythemes)
+library(reticulate)
+
+ReadCsvParam <- function(inFile)
+{
+  if(is.null(inFile))
+    return(NULL)
+  else
+    return(read_csv(inFile$datapath))
+}
+
+UpdateProgress <- function(renderVal, renderByShiny)
+{
+  if(renderByShiny & renderVal < 25)
+  {
+    shiny::setProgress(renderVal/25)
+    renderVal = renderVal + 1
+  } 
+}
